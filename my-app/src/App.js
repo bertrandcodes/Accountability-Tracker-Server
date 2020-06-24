2dd59511e7e6f9fd014b6684d6f82e4d9c5199ac
Bertrand Shao <bertrandshao@Bertrands-MacBook-Pro.local>
2020-06-24 16:59:12 -0700

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      The accountability app
+    </div>
+  );
+}
+
+export default App;
